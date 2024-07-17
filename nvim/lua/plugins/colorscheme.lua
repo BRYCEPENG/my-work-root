a3342bb1fe30109230f990c1f4c2d962618965de
pedro.peng <pp0755@hotmail.com>
2024-07-17 20:48:44 +0800

    Update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,11 +2,16 @@ return {
   -- From: https://github.com/savq/melange-nvim
   {
     "savq/melange-nvim",
+    -- enabled = false,
     config = function()
       vim.opt.termguicolors = true
       vim.cmd.colorscheme("melange")
       -- Default using with dark background
       -- vim.opt.background = "light"
     end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    enabled = false,
   },
 }
