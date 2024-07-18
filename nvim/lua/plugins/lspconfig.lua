7875cc41e4b78970cf8fe80d295af3fde8e8a14b
pedro.peng <pp0755@hotmail.com>
2024-07-18 21:32:43 +0800

    Using 'md' 'mr'

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -4,9 +4,8 @@ return {
     opts = function()
         local keys = require("lazyvim.plugins.lsp.keymaps").get()
         keys[#keys + 1] = {'gi', "<cmd>Lspsaga finder imp<CR>"}
-        keys[#keys + 1] = {'gi', "<cmd>Lspsaga finder imp<CR>"}
-        keys[#keys + 1] = {'gd', "<cmd>Lspsaga peek_definition<CR>"}
-        keys[#keys + 1] = {'gr', "<cmd>Lspsaga finder ref+imp<CR>"}
+        keys[#keys + 1] = {'md', "<cmd>Lspsaga peek_definition<CR>"}
+        keys[#keys + 1] = {'mr', "<cmd>Lspsaga finder ref+imp<CR>"}
         keys[#keys + 1] = {'K', "<cmd>Lspsaga hover_doc<CR>"}
     end,
 }
