1f20a7d50767df130cc6d45d9fa19e94a49df976
pedro.peng <pp0755@hotmail.com>
2024-07-19 16:06:57 +0800

    Update.....

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,12 +2,20 @@ return {
   -- From: https://github.com/savq/melange-nvim
   {
     "savq/melange-nvim",
-    -- enabled = false,
+    enabled = false,
     config = function()
       vim.opt.termguicolors = true
       vim.cmd.colorscheme("melange")
       -- Default using with dark background
       -- vim.opt.background = "light"
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      vim.opt.termguicolors = true
+      -- vim.cmd.colorscheme("kanagawa-wave")
+      vim.cmd.colorscheme("kanagawa-dragon")
     end,
   },
   {
