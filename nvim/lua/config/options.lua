989accbcc50982b83e18acc047b4385064eaf908
pedro.peng <pp0755@hotmail.com>
2024-07-22 15:35:43 +0800

    Update......

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -9,3 +9,4 @@ vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a T
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 vim.o.pumblend = 0 -- Special for Tebby Terminal.
+vim.o.numberwidth = 7 -- Set numberwidth to fix flick issue.
